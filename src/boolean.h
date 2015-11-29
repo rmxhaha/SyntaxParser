053fcdbfa4bedc7ceb5f24ad45c0b0882b6281bf
rmxhaha <code000123@gmail.com>
2015-11-29 16:47:41 +0700

    boolean

---
+++ src/boolean.h
@@ -0,0 +1,12 @@
+
+#ifndef BOOLEAN_H
+#define BOOLEAN_H
+
+#define boolean unsigned char
+#define true 1
+#define false 0
+
+#define return_boolean(cond)\
+	if(cond) return true; else return false;
+
+#endif
