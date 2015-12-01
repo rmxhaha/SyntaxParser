bc7e16078aa29c231e5d2ade86ac7c9dab2a4f43
rmxhaha <code000123@gmail.com>
2015-12-01 21:19:17 +0700

    docs on symbol list

--- src/mesintoken.h
+++ src/mesintoken.h
@@ -12,6 +12,27 @@ typedef struct {
 	int line;
 } Token;
 
+/*
+R Error
+B Begin
+E End
+I Input
+O Output
+D do
+W while
+F if
+T then
+L else
+C conditional < > <= >=
+N Number
+M Comment
+Q khusus =
+A arthimatic
+L khusus (
+R khusus )
+V variable
+*/
+
 void assign_token_symbol( Token *t );
 
 void mtoken_init();
